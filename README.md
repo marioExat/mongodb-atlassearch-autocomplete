0394ad038d88575750905728891718d7bdc81cf8
MarioExat <you@example.com>
2024-02-09 14:43:36 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,38 @@
+MONGO DB ATLAS search sample app
+
+## Links
+
+- [Building an Autocomplete Form Element with Atlas Search and JavaScript](https://www.youtube.com/watch?v=3IDlOI0D8-8&t=830s)
+
+- [Creating a .env file](https://deadsimplechat.com/blog/environment-variables-in-nodejs/)
+
+## Setup
+
+1. Create a sample search index in atlas db with dyanmic mapping similar to this
+
+```
+{
+    "name": "sample_supplies-sales-dynamic",
+    "searchAnalyzer": "lucene.standard",
+    "analyzer": "lucene.standard",
+    "collectionName": "sales",
+    "database": "sample_supplies",
+    "mappings": {
+        "dynamic": true
+    }
+}
+```
+
+2. in the `$search` stage, set the index as the name of the search `index` field in atlas search, in this case it would be
+   `sample_supplies-sales-dynamic`.
+
+3. Create a .env file in root directory and add the following entries
+
+```
+DATABASE_URL=sample-mongo-url
+PORT=3001
+DATABASE_NAME=sample-database-name
+COLLECTION_NAME=sample-collection-name
+```
+
+4. If all else works fine, then test the file by running the app using `node main.js` and then go to the browser and query the server using the below url `http://localhost:3001/search?term=avvalakki`(change the port number if the running port is different).
---
+++ backend/main.js
@@ -0,0 +1,61 @@
+const { MongoClient } = require("mongodb");
+const Express = require("express");
+require("dotenv").config();
+const Cors = require("cors");
+const BodyParser = require("body-parser");
+const port = process.env.PORT || 3002;
+
+const server = Express();
+server.use(BodyParser.json());
+server.use(BodyParser.urlencoded({ extended: true }));
+server.use(Cors());
+const mongoURL = process.env.DATABASE_URL;
+const mongoDBName = process.env.DATABASE_NAME;
+const mongoCollectionName = process.env.COLLECTION_NAME;
+const client = new MongoClient(mongoURL);
+const spawn = require("child_process").spawn;
+
+server.get("/search", async (req, res) => {
+  try {
+    console.log(req.query.term);
+    let result = await collection
+      .aggregate([
+        {
+          $search: {
+            index: "default",
+            text: {
+              query: req.query.term,
+              path: {
+                wildcard: "*",
+              },
+              fuzzy: {
+                //maxEdits: 2,
+              },
+            },
+          },
+        },
+        {
+          $project: {
+            _id: 0,
+          },
+        },
+        {
+          $limit: 10,
+        },
+      ])
+      .toArray();
+    res.send(result);
+  } catch (error) {
+    response.status(500).send({ message: e.message });
+  }
+});
+server.listen(port, async () => {
+  try {
+    await client.connect();
+    collection = client.db(mongoDBName).collection(mongoCollectionName);
+    console.log(`Successfully running on port: ${port}`);
+    spawn("open", [`http://localhost:${port}/search?term=avvalakki`]);
+  } catch (err) {
+    console.log(err);
+  }
+});
